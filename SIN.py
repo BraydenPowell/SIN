fd2e1331d58b2cc639fe3af1dcdf3d8bb9fba3a2
BraydenPowell <86811660+BraydenPowell@users.noreply.github.com>
2021-07-14 03:57:22 +1000

    Update SIN.py

--- SIN.py
+++ SIN.py
@@ -6,26 +6,52 @@ currentPos = np.zeros(nInst)
 
 def getMyPosition(prcSoFar):
     global currentPos
-
     (nins,nt) = prcSoFar.shape
     prcSoFar = pd.DataFrame(prcSoFar)
-
     short_ema = prcSoFar.ewm(span=5, adjust=False, axis=1).mean()
-        # 5 day exponential moving average
-    posSize = 10000
-        #Max allowed size under competition rules
+    posSize = 2500
     buy = posSize
     sell = -posSize
+    buy_big = posSize*4
+    sell_big = -posSize*4
     rpos = np.zeros(nInst)
+    trade_count = np.zeros(nInst)
+    days = prcSoFar.shape[1]
+    curr_pos = np.zeros(nInst)
+    r_pos = np.zeros(nInst)
+    stock_pnl = np.zeros(nInst)
+    ev = np.zeros(nInst)
+    
+    
+    for n in range(days):
+        r_pos = np.zeros(nInst)
+        for m in range(100):
+            if prcSoFar.loc[m, n] < (short_ema.loc[m, n] * 0.991):
+                r_pos[m] += round(buy_big/prcSoFar.loc[m, n])
+            if prcSoFar.loc[m, n] > (short_ema.loc[m, n] * 1.009):
+                r_pos[m] += round(sell_big/prcSoFar.loc[m, n])
+            stock_pnl[m] += curr_pos[m] * prcSoFar.loc[m, n] - curr_pos[m] * prcSoFar.iloc[m, n-1]
+                
+        if n > 1:
+            for j in range(0,100):
+                if curr_pos[j]*prcSoFar.loc[j,n] != curr_pos[j]*prcSoFar.loc[j, n-1]:
+                    trade_count[j] += 1
+            
+        curr_pos = r_pos
 
     for i in range(100):
-        # opens net long position the instrument if the price is far enough below the 5 day ema to cover commission if the price reverts
-        if prcSoFar.iloc[i, -1] < (short_ema.iloc[i, -1] * 0.991):
-            rpos[i] += round(buy/prcSoFar.iloc[i, -1])
-        # opens net short position the instrument if the price is far enough below the 5 day ema to cover commission if the price reverts
-        if prcSoFar.iloc[i, -1] > (short_ema.iloc[i, -1] * 1.009):
-            rpos[i] += round(sell/prcSoFar.iloc[i, -1])
-    
+        if trade_count[i] > 0:
+            ev[i] = stock_pnl[i] / trade_count[i]
+        if prcSoFar.loc[i, nt-1] < (short_ema.loc[i, nt-1] * 0.991):
+            if ev[i] == 0:
+                rpos[i] += round(buy/prcSoFar.loc[i, nt-1])
+            if ev[i] > 0:
+                rpos[i] += round(buy_big/prcSoFar.loc[i, nt-1])
+        if prcSoFar.loc[i, nt-1] > (short_ema.loc[i, nt-1] * 1.009):
+            if ev[i] == 0:
+                rpos[i] += round(sell/prcSoFar.loc[i, nt-1])
+            if ev[i] > 0:
+                rpos[i] += round(sell_big/prcSoFar.loc[i, nt-1])
+                
     currentPos = rpos
-    # adapts the currentPos with the new position updates, defaults to no position without a strategy trigger
     return currentPos
