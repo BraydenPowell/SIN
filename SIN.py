533cbfe7dda1527c25ee369deb67421d2c10a899
BraydenPowell <86811660+BraydenPowell@users.noreply.github.com>
2021-07-13 20:40:22 +1000

    Update SIN.py

--- SIN.py
+++ SIN.py
@@ -1,1 +1,31 @@
+import pandas as pd
+import numpy as np
 
+nInst = 100
+currentPos = np.zeros(nInst)
+
+def getMyPosition(prcSoFar):
+    global currentPos
+
+    (nins,nt) = prcSoFar.shaper
+    prcSoFar = pd.DataFrame(prcSoFar)
+
+    short_ema = prcSoFar.ewm(span=5, adjust=False, axis=1).mean()
+        # 5 day exponential moving average
+    posSize = 10000
+        #Max allowed size under competition rules
+    buy = posSize
+    sell = -posSize
+    rpos = np.zeros(nInst)
+
+    for i in range(100):
+        # opens net long position the instrument if the price is far enough below the 5 day ema to cover commission if the price reverts
+        if prcSoFar.iloc[i, -1] < (short_ema.iloc[i, -1] * 0.991):
+            rpos[i] += round(buy/prcSoFar.iloc[i, -1])
+        # opens net short position the instrument if the price is far enough below the 5 day ema to cover commission if the price reverts
+        if prcSoFar.iloc[i, -1] > (short_ema.iloc[i, -1] * 1.009):
+            rpos[i] += round(sell/prcSoFar.iloc[i, -1])
+    
+    currentPos = rpos
+    # adapts the currentPos with the new position updates, defaults to no position without a strategy trigger
+    return currentPos
